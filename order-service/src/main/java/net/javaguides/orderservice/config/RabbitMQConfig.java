85de328258a2d0c8babe0ecff614b14f47312ced
chrisann.lee <chrisannlee97@gmail.com>
2026-01-14 13:42:20 +0800

    first commit

---
+++ order-service/src/main/java/net/javaguides/orderservice/config/RabbitMQConfig.java
@@ -0,0 +1,56 @@
+package net.javaguides.orderservice.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+    // spring bean for queue -order queue
+    // spring bean for exchange
+    // spring bean for binding exchange and queue using routing key
+    // message converter
+    // configure RabbitTemplate
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.queue.name}")
+    private String orderQueue;
+
+    @Value("${rabbitmq.binding.routing.key}")
+    private String orderRoutingKey;
+
+    @Bean
+    public Queue queue() {
+        return new Queue(orderQueue);
+    }
+
+    public TopicExchange exchange() {
+        return new TopicExchange(exchange);
+    }
+
+    @Bean
+    public Binding binding() {
+        return BindingBuilder.bind(queue())
+                .to(exchange())
+                .with(orderRoutingKey);
+    }
+
+    @Bean
+    public MessageConverter converter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(converter());
+        return rabbitTemplate;
+    }
+}
